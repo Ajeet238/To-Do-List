9b6bf30f3d8815122dc8423b3f423aa78d0d4c5b
Ajeet238 <86551185+Ajeet238@users.noreply.github.com>
2021-07-29 13:54:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 add = document.getElementById("add");
 
 let update = function () {
-    tt = document.getElementById("title").value;
-    dd = document.getElementById("description").value;
+    var tt = document.getElementById("title").value;
+    var dd = document.getElementById("description").value;
+
+    // updating local storage   
     if (localStorage.getItem('list') == null) {
         console.log("updating list");
         list_arr = [];
-        list_arr.push([tt, dd]);
-        localStorage.setItem('list', JSON.stringify(list_arr));
+        if (dd != "" && tt != "") {
+            list_arr.push([tt, dd]);
+            localStorage.setItem('list', JSON.stringify(list_arr));
+        }
+
     }
     else {
         liststr = localStorage.getItem('list');
         list_arr = JSON.parse(liststr);
-        list_arr.push([tt, dd]);
-        localStorage.setItem('list', JSON.stringify(list_arr));
+        if (dd != "" && tt != "") {
+            list_arr.push([tt, dd]);
+            localStorage.setItem('list', JSON.stringify(list_arr));
+        }
     }
 
-
+    // updating list
     table_body = document.getElementById("bd");
     let str = "";
     list_arr.forEach((element, index) => {
@@ -25,13 +32,37 @@ let update = function () {
         <th scope="row">${index + 1}</th>
         <td>${element[0]}</td>
         <td>${element[1]}</td>
-        <td><button class="btn btn-primary" id="del">Delete</button></td>
+        <td><button class="btn btn-primary" id="del" onclick="del(${index})">Delete</button></td>
         </tr>
         `;
     });
     table_body.innerHTML = str;
-;}
+    ;
+}
 update();
-
-
+// activate add to list buttun
 add.addEventListener("click", update);
+
+// deleting list row
+
+let del = function (item) {
+    
+    console.log("deleting " + item)
+    liststr = localStorage.getItem('list');
+    list_arr = JSON.parse(liststr);
+    list_arr.splice(item, 1);
+    localStorage.setItem('list', JSON.stringify(list_arr));
+
+    location.reload();
+    
+}
+
+// reset buttun
+
+let reset = function(){
+    if( confirm("Are You Sure To Reset The List?"))
+   
+    localStorage.clear();
+    location.reload();
+   // update();
+}
